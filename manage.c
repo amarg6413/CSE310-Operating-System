e3414d2441a01103acdc111a839f0457ddb93064
amarg6413 <47099838+amarg6413@users.noreply.github.com>
2020-03-27 23:18:46 +0530

    Add files via upload

--- manage.c
+++ manage.c
@@ -6,7 +6,10 @@
 #define MAX_PID 1000
 #define MIN_PID 100
 const int max_size = MAX_PID - MIN_PID;
+
 int map[900]={0};
+
+
 int allocate_map(){
     printf("Started marking every bitmap free");
     int i=0;
@@ -16,11 +19,13 @@ int allocate_map(){
     printf("Successfully marked");
     return 1;
 }
+
 int allocate_pid(){
     int i=0;
     while(i<max_size){
         if(map[i]==0){
             map[i]=1;
+            printf("Allocated PID %d\n",MIN_PID+i);
             return MIN_PID+i;
         }
         else
@@ -33,6 +38,7 @@ int allocate_pid(){
         }
     }
 }
+
 
 void release_pid(int pid){
     if(pid<1000 &&pid>=100)
@@ -47,6 +53,7 @@ void release_pid(int pid){
     
 }
 
+
 void *ThreadFun(void *threadid) {
     int tid,pid;
     tid=*((int *)threadid);
@@ -58,26 +65,26 @@ void *ThreadFun(void *threadid) {
     else
     {
     int n = rand() % 31 + 1;
-    printf("Thread ID, %d of PID %d created and sleeping for %d sec\n", tid,pid,n);
+    printf("Thread , %d of PID %d created and sleeping for %d sec\n", tid,pid,n);
     sleep(n);
-    printf("Thread ID, %d of PID %d released in %d seconds\n", tid,pid,n);
+    printf("Thread , %d of PID %d released in %d seconds\n", tid,pid,n);
     release_pid(pid);
     pthread_exit(NULL);
     }
 }
+
+
 int main () {
     int a=allocate_map();
     if(a==1)
     {
-    printf("Enter the no of threads you want:-");
+    printf("Enter the no of processes you want:-");
     int n;
     scanf("%d",&n);
     pthread_t threads[n];
     int i;
     for( i = 0; i < n; i++ ) {
-        printf("Creating Process %d\n",i);
-        pthread_create(&threads[i], NULL, ThreadFun,(void*)&i);
-        
+        pthread_create(&threads[i], NULL, ThreadFun,(void*)&i);//creating the threads 
         }
     for(i=0; i<n; i++)
         pthread_join(threads[i],NULL);    
@@ -92,10 +99,11 @@ int main () {
         pthread_join(threads[i],NULL);
     }
     ***/
-    else
+    if(a!=1)
     {
         printf("Error while allocating the map");
     }
     
 
+}
 }
